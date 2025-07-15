76a62e49cdb3d94f3932247ca11f89285c3e19f4
maxrxsugar <max@rxsugar.com>
2025-07-15 13:53:47 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
-# build.sh
-
-# Create and activate virtual env
 python -m venv .venv
 source .venv/bin/activate
-
-# Clean install deps
 pip install --upgrade pip
 pip install -r requirements-temp.txt
 
